793589a5db254bb73365f4dfe2e3fcdb5498ce70
Rayco Sánchez García <raycosanchez@gmail.com>
2014-11-23 23:27:40 +0100

    Some refactoring done on Cell methods

--- Cell.cpp
+++ Cell.cpp
@@ -20,9 +20,7 @@ void Cell::nextGeneration()
 std::list<State> Cell::getNeighborsState()
 {
 	std::list<State> neighborsStateList;
-	std::list<State>::const_iterator neighborsStateIterator;
-	std::list<Cell>::const_iterator neighborsIterator;
-	for (neighborsIterator = neighbors.begin(); neighborsIterator != neighbors.end(); ++neighborsIterator)
+	for (std::list<Cell>::const_iterator neighborsIterator = neighbors.begin(); neighborsIterator != neighbors.end(); ++neighborsIterator)
 		neighborsStateList.push_back(neighborsIterator->getState());
 	return neighborsStateList;
 }
@@ -30,11 +28,7 @@ std::list<State> Cell::getNeighborsState()
 int Cell::getAliveNeighbors(std::list<State> neighborsStateList) 
 {
 	int aliveNeighbors = 0;
-	std::list<State>::const_iterator iterator;
-	for (iterator = neighborsStateList.begin(); iterator != neighborsStateList.end(); ++iterator)
-	{
-		if (State::ALIVE == (*iterator))
-			++aliveNeighbors;
-	}
+	for (std::list<State>::const_iterator iterator = neighborsStateList.begin(); iterator != neighborsStateList.end(); ++iterator)
+		if (State::ALIVE == (*iterator)) ++aliveNeighbors;
 	return aliveNeighbors;
 }
