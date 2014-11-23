445df458ec40036fffda9b2a0eef908594abbdb4
Rayco Sánchez García <raycosanchez@gmail.com>
2014-11-23 17:08:36 +0100

    Test that checks if a dead cell keeps dead with two alive neighbors passed.

--- RulesTests.cpp
+++ RulesTests.cpp
@@ -9,36 +9,37 @@ namespace GameOfLife
 	TEST_CLASS(RulesTests)
 	{
 	private:
-		State getNextState(int aliveNeighbors) {
-			if ((aliveNeighbors == 2) || (aliveNeighbors == 3))
-				return State::ALIVE;
+		State getNextState(State actualState, int aliveNeighbors) {
+			if (State::ALIVE == actualState)
+				if ((aliveNeighbors == 2) || (aliveNeighbors == 3))
+					return State::ALIVE;
 			return State::DEAD;
 		}
 	public:
 		
 		TEST_METHOD(AliveCellDiesWithMoreThan3AliveNeighbors)
 		{
-			Assert::AreEqual(State::DEAD, getNextState(4)); 
+			Assert::AreEqual(State::DEAD, getNextState(State::ALIVE, 4)); 
 		}
 
 		TEST_METHOD(AliveCellDiesWithLessThan2AliveNeighbors)
 		{
-			Assert::AreEqual(State::DEAD, getNextState(1)); 
+			Assert::AreEqual(State::DEAD, getNextState(State::ALIVE, 1)); 
 		}
 
 		TEST_METHOD(AliveCellSurviveWith2AliveNeighbors)
 		{
-			Assert::AreEqual(State::ALIVE, getNextState(2)); 
+			Assert::AreEqual(State::ALIVE, getNextState(State::ALIVE, 2)); 
 		}
 
 		TEST_METHOD(AliveCellSurviveWith3AliveNeighbors)
 		{
-			Assert::AreEqual(State::ALIVE, getNextState(3)); 
+			Assert::AreEqual(State::ALIVE, getNextState(State::ALIVE, 3)); 
 		}
 
 		TEST_METHOD(DeadCellKeepsDeadWith2AliveNeighbors)
 		{
-			Assert::AreEqual(State::DEAD, getNextState(2)); 
+			Assert::AreEqual(State::DEAD, getNextState(State::DEAD, 2)); 
 		}
 	};
 }
